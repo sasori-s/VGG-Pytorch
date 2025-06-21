efb080c059624e8501f3c454ef40b5f68089f3c3
sasori-s <shams.iqbal@g.bracu.ac.bd>
2025-06-22 02:11:57 +0600

    Transform pipeline, after a lot of research

--- preprocessing.py
+++ preprocessing.py
@@ -19,21 +19,24 @@ class Preprocess(datasets.ImageFolder):
             image_size : int,
             single_image : bool = False
     ) -> None: 
+        
         self.data_path = data_path
         self.image_size = image_size
+        self.scaling_image_size = 256
         self.transformations()
         # self.show_images()
         super(Preprocess, self).__init__(data_path, transform=self.transforms)
-        self.dataloader = DataLoader(self, batch_size=32, shuffle=True)
+        self.dataloader = DataLoader(self, batch_size=128, shuffle=True, num_workers=3)
         
 
     def transformations(self) -> None:
         self.transforms = v2.Compose([
-            v2.Resize(size=(self.image_size, self.image_size), antialias=True),
             v2.ToImage(),
-            v2.RandomResizedCrop(self.image_size, antialias=True),
-            v2.RandomHorizontalFlip(p=0.5),
-            v2.ToDtype(torch.float32, scale=True)
+            v2.ScaleJitter(target_size=(self.scaling_image_size, self.scaling_image_size), scale_range=(0.1, 2.0)),
+            v2.RandomCrop(size=self.image_size),
+            v2.RandomHorizontalFlip(),
+            v2.ToDtype(dtype=torch.float32),
+            v2.Normalize(mean=[0.5087, 0.5006, 0.4405], std=[0.2832, 0.2682, 0.2887])
         ])
 
 
@@ -65,6 +68,10 @@ class Preprocess(datasets.ImageFolder):
         single_image_path = os.path.join(self.data_path, images[1])
         self.load_reshaped_image(single_image_path)
         print(len(os.listdir(self.data_path)))
+
+
+class MultiScalingScaling():
+    pass
 
 
 if __name__ == '__main__':
