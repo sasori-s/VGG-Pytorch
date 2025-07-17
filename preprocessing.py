8d9404d8759ade25187f3e3b4caa4a64b095a190
sasori-s <shams.iqbal@g.bracu.ac.bd>
2025-07-17 17:28:44 +0600

    Trying to load less class to see the distribution

--- preprocessing.py
+++ preprocessing.py
@@ -16,23 +16,20 @@ class Preprocess(datasets.ImageFolder):
     def __init__(
             self, 
             data_path : str, 
-            image_size : int,
-            single_image : bool = False
+            scale_size : int = 256,
     ) -> None: 
         
         self.data_path = data_path
-        self.image_size = image_size
-        self.scaling_image_size = 256
-        self.transformations()
-        # self.show_images()
-        super(Preprocess, self).__init__(data_path, transform=self.transforms)
-        self.dataloader = DataLoader(self, batch_size=128, shuffle=True, num_workers=3)
+        self.image_size = 224
+        self.scale_size = scale_size
+        self.stack_transformations_multiscale()
+        super(Preprocess, self).__init__(data_path, transform=self.training_transformations)
+        self.dataloader = DataLoader(self, batch_size=128, shuffle=True, num_workers=3, pin_memory=True)
         
 
-    def transformations(self) -> None:
-        self.transforms = v2.Compose([
-            v2.ToImage(),
-            v2.ScaleJitter(target_size=(self.scaling_image_size, self.scaling_image_size), scale_range=(0.1, 2.0)),
+    def stack_transformations_multiscale(self) -> None:
+        self.training_transformations = v2.Compose([
+            v2.ScaleJitter(target_size=(self.scale_size, self.scale_size), scale_range=(0.1, 2.0)),
             v2.RandomCrop(size=self.image_size),
             v2.RandomHorizontalFlip(),
             v2.ToDtype(dtype=torch.float32),
@@ -40,39 +37,20 @@ class Preprocess(datasets.ImageFolder):
         ])
 
 
-    def load_reshaped_image(
-            self, image_path : str
-    ) -> Image:
-        image = Image.open(image_path)
-        # image = image.resize((self.image_size, self.image_size), Image.BILINEAR)
-        # image = v2.Resize(size = (self.image_size, self.image_size), antialias=True)(image)
-        plt.axis('off')
-        plt.imshow(image)
-        plt.savefig("reshaped_image.png")
-        plt.show()
+    def stack_transformations_singlescale(self):
+        self.training_transformations = v2.Compose[
+            v2.RandomCrop(size=self.image_size),
+            v2.RandomHorizontalFlip(),
+            v2.ToDtype(dtype=torch.float32),
+            v2.Normalize(mean=[0.5087, 0.5006, 0.4405], std=[0.2832, 0.2682, 0.2887])
+        ]
 
-        transformed_image = self.transforms(image)
-        tensor_to_image = transformed_image.numpy().transpose(1, 2, 0)
-        plt.imshow(tensor_to_image)
-        plt.savefig("transformed_image.png")
-
-        tensor_image = v2.ToImage()(image)
-        tensor_to_image = tensor_image.numpy().transpose(1, 2, 0)
-        plt.imshow(tensor_to_image)
-        plt.savefig("tensor_img.png")
-        return image
+    def creating_datasets_and_dataloader(self):
+        pass
+        
     
-
-    def show_images(self) -> None:
-        images = os.listdir(self.data_path)
-        single_image_path = os.path.join(self.data_path, images[1])
-        self.load_reshaped_image(single_image_path)
-        print(len(os.listdir(self.data_path)))
-
-
-class MultiScalingScaling():
-    pass
+    
 
 
 if __name__ == '__main__':
-    preproces = Preprocess("/mnt/A4F0E4F6F0E4D01A/Shams Iqbal/VS code/Kaggle/Datasets/animal_dataset/animals/animals", 256)
+    preproces = Preprocess("/mnt/A4F0E4F6F0E4D01A/Shams Iqbal/VS code/Kaggle/Datasets/CIFAR-100-dataset/train", 256)
