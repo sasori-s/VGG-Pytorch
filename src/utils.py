33b712f19d42b832d5ceb97287970a432517dbbd
sasori-s <shams.iqbal@g.bracu.ac.bd>
2025-08-12 12:16:46 +0600

    added resuming training functionality

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,9 @@
+# All creadit goes to https://github.com/Lornatang/VGG-PyTorch/blob/main/utils.py
+
 import torch
 
+
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 def accuracy(output, target, top_k=(1, )):
     with torch.no_grad():
@@ -28,4 +32,30 @@ def load_state_dict(model, state_dict):
     model.load_state_dict(model_state_dict)
 
     return model
+
+
+def load_pretrained_state_dict(model, model_weight_path):
+    checkpoints = torch.load(model_weight_path, map_location=DEVICE)
+    model = load_state_dict(model, checkpoints["state_dict"])
+
+    return model
+
+
+def load_resume_state_dict(model, 
+                        model_weight_path, 
+                        new_model, 
+                        optimizer, 
+                        scheduler):
+    checkpoint = torch.load(model_weight_path, map_location=DEVICE)
+
+    start_epoch = checkpoint["epoch"]
+    best_acc = checkpoint["best_acc1"]
+
+    model = load_state_dict(model, checkpoint["state_dict"])
+    new_model = load_state_dict(new_model, checkpoint["new_model_state_dict"])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    scheduler.load_state_dict(checkpoint['scheduler'])
+
+    return model, new_model, start_epoch, best_acc, optimizer, scheduler
+
 
