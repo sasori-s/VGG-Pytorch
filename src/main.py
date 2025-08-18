7b649230229fe92ee93dc055dab4ca9be4b68694
sasori-s <shams.iqbal@g.bracu.ac.bd>
2025-08-18 18:02:27 +0600

    Modifying to start training from the main.py file

--- src/main.py
+++ src/main.py
@@ -2,8 +2,48 @@ from model import VGG11
 from train import Training
 from preprocessing import Preprocess
 from precompute_mean_std import PreCompute
+import torch.nn as nn
+import torch
+from torch.optim.lr_scheduler import LRScheduler
 
-def main(dataset_path):
-    preprocessing = Preprocess(dataset_path, scale_size=256, purpose='training')
-    dataloader = preprocessing()
-    trainer = Training(dataloader)
+DATASET_PATH = "/mnt/A4F0E4F6F0E4D01A/Shams Iqbal/VS code/Kaggle/Datasets/CIFAR-100-dataset/"
+
+def initiate_training_parameters():
+    train_preprocessor = Preprocess(DATASET_PATH + "train", scale_size=256, purpose='training')
+    val_preprocessor = Preprocess(DATASET_PATH + 'test')
+
+    train_dataloader = train_preprocessor()
+
+    criterion = nn.CrossEntropyLoss()
+
+    model = VGG11(224, num_classes=100)
+    optimizer_sgd = torch.optim.SGD(model.parameters(), lr=1e-2, weight_decay=5 * 10e-4, momentum=0.9)
+    optimizer_adam = torch.optim.Adam(model.parameters(), lr=1e-2)
+    lr_scheduler = LRScheduler(optimizer_sgd)
+    epochs = 50
+    batch_size = 64
+
+    model_kwgs = {
+        "model" : model,
+        "dataloader": train_dataloader,
+        "epochs" : epochs,
+        "criterion" : criterion,
+        "optimizer" : optimizer_sgd,
+        "lr_scheduler" : lr_scheduler,
+        "batch_size" : batch_size,
+
+    } 
+
+    return model_kwgs
+    
+
+def main():
+    kwgs = initiate_training_parameters()
+    trainer = Training(**kwgs)
+    
+
+if __name__ == '__main__':
+    main()
+    
+
+
--- src/train.py
+++ src/train.py
@@ -10,11 +10,35 @@ from torch.optim.lr_scheduler import LRScheduler
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 class Training(nn.Module):
-    def __init__(self, dataloader):
+    def __init__(self, dataloader, batch_size, model, epochs, criterion, optimizer, scheduler, data_path):
         self.dataloader = dataloader
-        self.model = VGG11(224 ,num_classes=100)
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer_sgd = torch.optim.SGD(self.model.parameters(), lr=1e-2, weight_decay=5 * 10e-4, momentum=0.9)
-        self.optimizer_adam = torch.optim.Adam(self.model.parameters(), lr=1e-2)
-        self.scheduler = LRScheduler(self.optimizer)
+        self.model = model
+        self.model_name = model.__class__.__name__
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler(self.optimizer)
+
+
+        self.train_loss = []
+        self.val_loss = []
+
+        self.train_accuracy = []
+        self.val_accuracy = []
+
+        self.best_loss = float("inf")
+        self.best_accuracy = 0.0
+
+
+    def train_model(self):
+        pass
+
+
+    def validate_model(self):
+        pass
+
+    
+    def training_process(self):
+        self.training_data = self.dataloader()
 
